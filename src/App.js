719f308d181231e8319fe0bac69b17fb748c8add
Omar Morales <omar.ortega@oracle.com>
2021-04-19 22:22:44 -0500

    Show beers in index from beer api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import beer from "./assets/img/beer.svg";
 import "./App.css";
 
 function App() {
+  
+const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/v1/beer/")
+    .then(res => {
+      const beersData = res.data;
+      setBeers(beersData)
+      console.log(beersData)
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={beer} className="App-logo" alt="logo" />
-        <p>This is a cool app in progress</p>
+        {beers.map(b => {
+
+          const { name, type, country, quantity } = b;
+          return (
+            <div>
+            <img src={beer} className="App-logo" alt="logo" />
+            <h4>{name} - <span style={{"font-style": "italic"}}>{type}</span></h4>
+            <p>{quantity} beers available</p>
+            </div>
+          )
+
+        })}
+        
       </header>
     </div>
   );
